ff50214e0a79fdc20f2012ac289ed83931d995e5
tanakamariko <gshm10218@gmail.com>
2016-04-15 20:30:23 +0900

    add knapsacTestData.txt

--- moea_d_lesson/src/moea_d_lesson/Main.java
+++ moea_d_lesson/src/moea_d_lesson/Main.java
@@ -5,6 +5,8 @@ public class Main {
   public static void main(String[] args) {
     // TODO 自動生成されたメソッド・スタブ
 
+  DataHttpUtils.fileGetContents( "ftp://ftp.tik.ee.ethz.ch/pub/people/zitzler/TESTDATA/ZT1999a/knapsack.500.2" );
+
   }
 
 }
